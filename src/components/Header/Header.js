77561225ce0ee210b99ade7878539fbc25c24271
Patrick Eitel <patrickeitel12@gmail.com>
2023-12-01 10:58:28 -0800

    Add Header.js component, and Header.css styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,28 @@
 // Header.js
+
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+return (
+  <nav>
+  <input type="checkbox" id="nav-toggle" />
+  <div class="logo">MINZ<strong>CODE</strong></div>
+  <ul class="links">
+     <li><a href="#home">Home</a></li>
+     <li><a href="#about">About</a></li>
+     <li><a href="#work">Work</a></li>
+     <li><a href="#projects">Projects</a></li>
+     <li><a href="#contact">Contact</a></li>
+  </ul>
+  <label for="nav-toggle" class="icon-burger">
+     <div class="line"></div>
+     <div class="line"></div>
+     <div class="line"></div>
+  </label>
+  </nav>
+  )
+}
+
+export default Header
