90063ac64f501da4b54464f67ef9ebb1937ca30b
Prissilla Escobar <prissilla988@gmail.com>
2023-12-07 11:40:20 -0700

    Create loading component and style

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,1 +1,14 @@
-// Loading.js
+import './LoadingComponent.css'
+import { TailSpin } from 'react-loading-icons'
+
+function Loading() {
+
+    return (
+        <div className='loading-container'>
+            <TailSpin />
+            <h1 className='loading-text'>Loading...</h1>
+        </div>
+    )
+}
+
+export default Loading
