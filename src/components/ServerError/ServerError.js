48079cd22c3649017b2bda1cfca713c07de49752
Prissilla Escobar <prissilla988@gmail.com>
2023-12-07 12:08:34 -0700

    Change image

--- src/components/ServerError/ServerError.js
+++ src/components/ServerError/ServerError.js
@@ -8,7 +8,7 @@ function ServerError({ resetError, serverError}) {
 
     return (
         <div className='serverError'>
-            <img className='error-image' alt='monopoly bankruptcy card' src='https://static.wikia.nocookie.net/monopoly/images/9/90/Bankruptcy_monopoly.jpg/revision/latest?cb=20130910230033' /> 
+            <img className='error-image' alt='monopoly bankruptcy card' src='https://render.fineartamerica.com/images/rendered/default/greeting-card/images/artworkimages/medium/3/chance-bankruptcy-card-vintage-monopoly-design-turnpike.jpg?&targetx=0&targety=0&imagewidth=700&imageheight=500&modelwidth=700&modelheight=500&backgroundcolor=280F08&orientation=0' /> 
             <p className='error-message'>Oh no! {errorMessage}</p>
             <Link to={'/home'} onClick={() => {resetError()}} className='home-link'>
                 <button className='return-button'>Return Home</button>
