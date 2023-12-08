936019acf73e9268e8b5bdb1f361c7e27f4ab7c9
Patrick Eitel <patrickeitel12@gmail.com>
2023-12-07 17:01:29 -0800

    Achieve base functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,44 @@
 import './Header.css';
-import Logo from '../../Logo.png'
-import { Link, useParams } from 'react-router-dom';
+import Logo from '../../Logo.png';
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
-function Header( {resetError, currentUser} ) {
+function Header({ resetError, currentUser }) {
+  const location = useLocation();
 
-return (
-  <nav>
-  <input type="checkbox" id="nav-toggle" />
-  <img className='logo' src={Logo} />
- <ul class="links">
-  <Link to={`/${currentUser}/home`}><li>Home</li></Link>
-  <Link to={`/${currentUser}/saved`}><li>Saved Games</li></Link>
-     {/* <li><a href="#projects">All Games</a></li> */}
- </ul>
- <label for="nav-toggle" class="icon-burger">
-     <div class="line"></div>
-     <div class="line"></div>
-     <div class="line"></div>
- </label>
-</nav>
-  )
+  useEffect(() => {
+    if (location.pathname === '/') {
+      document.body.classList.remove("links");
+    }
+  }, [location]);
+
+  const isBasePath = location.pathname === '/';
+  const isSavedPath = location.pathname === `/${currentUser}/saved`;
+
+  return (
+    <nav>
+      <input type="checkbox" id="nav-toggle" />
+      <img className='logo' src={Logo} alt="Logo" />
+      <ul className="links">
+        <li>
+          <Link to={`/${currentUser}/home`} onClick={() => {resetError()}}>Home</Link>
+        </li>
+        {!isBasePath && !isSavedPath && (
+          <>
+            <li>
+              <Link to={`/${currentUser}/saved`}>Saved Games</Link>
+            </li>
+            {/* Additional menu items */}
+          </>
+        )}
+      </ul>
+      <label htmlFor="nav-toggle" className="icon-burger">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </label>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,7 @@
 import './Users.css'
 import UserCard from "../UserCard/UserCard"
 
+
 function Users( {users, setCurrentUser} ) {
   const allUsers = users.map((user) => {
     return (
@@ -9,6 +10,7 @@ function Users( {users, setCurrentUser} ) {
       id={user.data.id}
       name={user.data.attributes.name}
       setCurrentUser={setCurrentUser}
+      img={'https://nypost.com/wp-content/uploads/sites/2/2022/10/jack-sparrow-39.jpg?quality=75&strip=all'}
     />
     )
   })
