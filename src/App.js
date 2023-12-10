8dae698dff23bf873b49dd531758ddd6fd0acb43
Prissilla Escobar <prissilla988@gmail.com>
2023-12-09 17:05:51 -0700

    Cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
-import './App.css';
-import { getBoardGames, getUsers } from './apiCalls';
-import Carousels from "./components/Carousel/Carousels";
-import Header from "./components/Header/Header";
-import SelectedGame from "./components/SelectedGame/SelectedGame";
+import { Routes, Route } from "react-router-dom"
+import { useState, useEffect } from "react"
+import './App.css'
+import { getBoardGames, getUsers } from './apiCalls'
+import Carousels from "./components/Carousel/Carousels"
+import Header from "./components/Header/Header"
+import SelectedGame from "./components/SelectedGame/SelectedGame"
 import Users from "./components/Users/Users"
-import mockUsers from "./mockUsers";
-import ServerError from "./components/ServerError/ServerError";
-import LoadingComponent from "./components/Loading/Loading";
+import ServerError from "./components/ServerError/ServerError"
+import LoadingComponent from "./components/Loading/Loading"
 import SavedGames from "./components/SavedGames/SavedGames"
-import mockGames from "./mockGames";
 
 function App() {
   const [games, setGames] = useState([])
   const [serverError, setServerError] = useState({hasError: false, message: ''})
-  const [isLoggedIn, setIsLoggedIn] = useState(false) // might want this for later?
   const [users, setUsers] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [currentUser, setCurrentUser] = useState(null)
+
+  console.log(users)
 
   useEffect(() => {
     getBoardGames()
