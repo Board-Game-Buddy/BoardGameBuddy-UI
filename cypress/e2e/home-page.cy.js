37bf3885a8792049fa6ac28ad6d33d9186be3535
Patrick Eitel <patrickeitel12@gmail.com>
2023-12-09 22:12:53 -0800

    update home page test

--- cypress/e2e/home-page.cy.js
+++ cypress/e2e/home-page.cy.js
@@ -16,7 +16,12 @@ describe('Home Page', () => {
     cy.get('nav')
       .find('.links > :nth-child(1) > a').should('exist');
     cy.get('.users-container').children()
-    .should('have.length', 3); // CHANGE THIS TO HOWEVER MANY USERS WE END UP GOING WITH
-    
+    .should('have.length', 3); // CHANGE THIS TO HOWEVER MANY USERS WE END UP GOING WITH THEN UNCOMMENT THE LAST 3 USERS!
+    cy.get('.users-container > :nth-child(1)').find('.name').should('have.text', 'Reed Hillmar');
+    cy.get('.users-container > :nth-child(2)').find('.name').should('have.text', 'Noelle Hemphill');
+    cy.get('.users-container > :nth-child(3)').find('.name').should('have.text', 'Connor Richmond');
+    // cy.get('.users-container > :nth-child(4)').find('.name').should('have.text', 'Lane Bretschneider');
+    // cy.get('.users-container > :nth-child(5)').find('.name').should('have.text', 'Prissilla Escobar');
+    // cy.get('.users-container > :nth-child(6)').find('.name').should('have.text', 'Patrick Eitel');
   });
 });
