77debb558e4b95245f3b5ef0594e52f8d439e797
Prissilla Escobar <prissilla988@gmail.com>
2023-12-09 23:58:25 -0700

    Edit return home button to link to the users home carousel page

--- src/components/ServerError/ServerError.js
+++ src/components/ServerError/ServerError.js
@@ -2,7 +2,7 @@ import './ServerError.css'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-function ServerError({ resetError, serverError}) {
+function ServerError({ resetError, serverError, currentUser}) {
 
     const errorMessage = serverError && serverError.message ? serverError.message : 'Unknown error occurred.'
 
@@ -10,7 +10,7 @@ function ServerError({ resetError, serverError}) {
         <div className='serverError'>
             <img className='error-image' alt='monopoly bankruptcy card' src='https://render.fineartamerica.com/images/rendered/default/greeting-card/images/artworkimages/medium/3/chance-bankruptcy-card-vintage-monopoly-design-turnpike.jpg?&targetx=0&targety=0&imagewidth=700&imageheight=500&modelwidth=700&modelheight=500&backgroundcolor=280F08&orientation=0' /> 
             <p className='error-message'>Oh no! {errorMessage}</p>
-            <Link to={'/'} onClick={() => {resetError()}} className='home-link'>
+            <Link to={`/${currentUser}/home`} onClick={() => {resetError()}} className='home-link'>
                 <button className='return-button'>Return Home</button>
             </Link>
         </div>
