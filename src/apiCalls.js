be05fdbb7c4230ac297e9b23b44a0e953b382fe4
Prissilla Escobar <prissilla988@gmail.com>
2023-12-10 22:47:43 -0700

    Cleaned up code :

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,17 +9,6 @@ export function getBoardGames() {
     )
   }
 
-export function getUsers() {
-  return fetch(`https://boardgamebuddy-api-a3b5bf335532.herokuapp.com/users/`).then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(`Users not found.`)
-      }
-     return response.json()
-    }
-  )
-}
-
 export function getSelectedGame(id) {
   return fetch(`https://middleman-api-8d134831a182.herokuapp.com/api/v1/board_games/${id}`)
   .then(response => {
@@ -34,8 +23,19 @@ export function getSearchedGames(searchCriteria) {
   return fetch(`https://middleman-api-8d134831a182.herokuapp.com/api/v1/board_games/all_by_params?${searchCriteria}`)
   .then(response => {
     if (!response.ok) {
-      throw new Error('Two player games not found.')
+      throw new Error('Category games not found.')
     }
     return response.json()
   })
+}
+
+export function getUsers() {
+  return fetch(`https://boardgamebuddy-api-a3b5bf335532.herokuapp.com/users`).then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(`Users not found.`)
+      }
+     return response.json()
+    }
+  )
 }
