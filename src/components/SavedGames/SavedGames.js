7b79e92c4c60d81fe0413ef6cdf54a1eedd3354d
Prissilla Escobar <prissilla988@gmail.com>
2023-12-10 23:00:24 -0700

    Move the useEffect to getUserFavorites to App and pass in as prop. Create state for rendered games and use async await to look at a users favorites, find an id and getSelectedGame and push that data into renderedGames. This is what is rendered as a GameCard. Set loading state and implemented redux

--- src/components/SavedGames/SavedGames.js
+++ src/components/SavedGames/SavedGames.js
@@ -2,44 +2,58 @@ import "./SavedGames.css"
 import PropTypes from 'prop-types';
 import GameCard from '../Card/GameCard'
 import { useSelector } from 'react-redux'
+import { useState, useEffect } from "react";
+import { getSelectedGame } from "../../apiCalls";
+import LoadingComponent from "../Loading/Loading";
 
-function SavedGames( {games, currentUser} ) {
-  const favoriteCards = useSelector((state) => state.favoriteCards)
+function SavedGames({ currentUser, setServerError, userFaves}) {
+  const idNum = parseInt(currentUser)
+  const favoriteCards = useSelector((state) => state.favoriteCards[currentUser] || [])
+  const [renderedGames, setRenderedGames] = useState([])
+  const [loading, setLoading] = useState(true)
 
-  console.log(favoriteCards)
+  useEffect(() => {
+    const renderGameCards = async () => {
+      const gamesToRender = []
 
-  const savedGames = games.map((game) => {
-    if (favoriteCards.includes(game.id)) {
-      return (
-        <GameCard
-          id={game.id}
-          key={game.key}
-          title={game.attributes.title}
-          categories={[game.attributes.categories]}
-          image={game.attributes.image_path}
-          description={game.attributes.description}
-          min_players={game.attributes.min_players}
-          max_players={game.attributes.max_players}
-          currentUser={currentUser}
-        />
-      )
+      for (const game of userFaves) {
+        try {
+          const selectedGameData = await getSelectedGame(game.id)
+          gamesToRender.push(
+            
+            <GameCard
+              id={selectedGameData.data.id}
+              key={selectedGameData.data.id}
+              title={selectedGameData.data.attributes.title}
+              image={selectedGameData.data.attributes.image_path}
+              currentUser={currentUser}
+              userFaves={userFaves}
+            />
+          )
+        } catch (error) {
+          console.error(`Error fetching selected game with ID ${game.id}:`, error)
+        }
+      }
+      setRenderedGames(gamesToRender)
     }
-    return null
-  })
-  
-  if (favoriteCards.length === 0) {
+    renderGameCards()
+    setLoading(false)
+  }, [userFaves, currentUser])
+
+  if (loading) {
+    return <LoadingComponent />
+  }
+
+  if (userFaves.length === 0) {
     return (
-      <div className='no-tracked'>
-          <h2 className='no-tracked-text'>You do not have any saved games.</h2>
-        </div>
+      <div className="no-tracked">
+        <h2 className="no-tracked-text">You do not have any saved games.</h2>
+      </div>
     )
   } else {
-      return (
-        <div className='savedgames'>
-          {savedGames}
-        </div>
-      )}
-} 
+    return <div className="savedgames">{renderedGames}</div>
+  }
+}
 
 export default SavedGames
 
