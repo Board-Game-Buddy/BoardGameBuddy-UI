54283b201460955112a3e871f55ca65774306605
Prissilla Escobar <prissilla988@gmail.com>
2024-01-22 13:52:08 -0700

    Create AddUserProfile form that will pop up when a user wants to create a new profile

---
+++ src/components/AddUserForm/AddUserForm.js
@@ -0,0 +1,11 @@
+import createUserProfile from '../../apiHooks'
+import { useState } from 'react'
+
+function AddUserProfileForm( addUser ) {
+
+    return (
+        <div></div>
+    )
+}
+
+export default AddUserProfileForm
